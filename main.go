35d2b953b68cb6d5bd6782de14a2e75bb17d514e
TimSalguz <timsalguz@gmail.com>
2024-02-27 22:28:05 +0200

    Release?

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
-    "fmt"
-    "strings"
-    "strconv"
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
+
 //Функция преобразует строку римского числа в целое.
 //Она не учитывает некоторые особенности римских чисел, и не является на 100% правильной!!!
 func rom_num_to_int(rom_num string) int {
@@ -84,11 +87,11 @@ func int_to_rom_num(num int) string {
 }
 
 func main() {
-    //var input string
-    input := "X - IX"
-    var num1_is_romanian, num2_is_romanian bool
-	//fmt.Print("Введите арифметическую опеарцию (разделяйте числа и арифметические операторы пробелами!): ")
-	//fmt.Scanln(&input)
+	fmt.Println("Введите математическую операцию. Между числами и операторами должны быть пробелы.")
+	input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+
+    var num1_is_roman, num2_is_roman bool
+
     parts := strings.Fields(input)
     if len(parts) != 3 {
         fmt.Println(len(parts));
@@ -97,21 +100,21 @@ func main() {
     num1, err := strconv.Atoi(parts[0])
 	if err != nil {
         num1 = rom_num_to_int(parts[0])
-        num1_is_romanian = true;
+        num1_is_roman = true;
 	}
 
 	num2, err := strconv.Atoi(parts[2])
 	if err != nil {
         num2 = rom_num_to_int(parts[2])
-        num2_is_romanian = true;
+        num2_is_roman = true;
 	}
     if !(num1 <= 10 && num2 <= 10){
         panic("Числа должны быть меньше или равны 10!")
     }
     answer_is_romanian := false;
-    if num1_is_romanian && num2_is_romanian{
+    if num1_is_roman && num2_is_roman{
         answer_is_romanian = true;
-    } else if !num1_is_romanian && !num2_is_romanian{
+    } else if !num1_is_roman && !num2_is_roman{
             answer_is_romanian = false;
     } else{
         panic("Римскими должны быть или все числа, или ни одно!");
@@ -140,10 +143,4 @@ func main() {
     } else{
         fmt.Println(result)
     }
-
-
-    // int_from_rom_num := rom_num_to_int(input)
-    // fmt.Println(int_from_rom_num)
-    // rom_num_from_int := int_to_rom_num(int_from_rom_num)
-    // fmt.Println(rom_num_from_int)
 }
